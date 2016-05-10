29bcd197544c8e41be059bd1f3a4350d07c9d1ae
Mc <zhaogw@asiainfo.com>
2016-05-10 20:38:47 +0800

    init

---
+++ src/main/webapp/jsp/register/register.jsp
@@ -0,0 +1,170 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java"%>
+<html>
+<head>
+<%@ include file="/inc/inc.jsp"%>
+<!--Support IE Text -->
+<meta http-equiv="X-UA-Compatible" content="IE=Edge" /> 
+<title>注册</title>
+<script type="text/javascript" src="${_base}/theme/baas/js/jquery.toggle-password.js" ></script> 
+
+<script type="text/javascript">
+ (function () {
+	seajs.use('app/register/register', function (RegisterPager) {
+		var pager = new RegisterPager();
+		pager.render();
+	});
+})(); 
+
+ $(function(){
+	$('#password').togglePassword({
+		el: '#togglePassword'
+	});
+}); 
+//判断输入密码的类型  
+function CharMode(iN){  
+if (iN>=48 && iN <=57) //数字  
+return 1;  
+if (iN>=65 && iN <=90) //大写  
+return 2;  
+if (iN>=97 && iN <=122) //小写  
+return 4;  
+else  
+return 8;   
+}  
+//bitTotal函数  
+//计算密码模式  
+function bitTotal(num){  
+modes=0;  
+for (i=0;i<4;i++){  
+if (num & 1) modes++;  
+num>>>=1;  
+}  
+return modes;  
+}  
+//返回强度级别  
+function checkStrong(sPW){  
+if (sPW.length<=8)  
+return 0; //密码太短  
+Modes=0;  
+for (i=0;i<sPW.length;i++){  
+//密码模式  
+Modes|=CharMode(sPW.charCodeAt(i));  
+}  
+return bitTotal(Modes);  
+}  
+ 
+//显示颜色  
+function pwStrength(pwd){  
+O_color="#eeeeee";  
+L_color="#FF0000";  
+M_color="#FF9900";  
+H_color="#33CC00";  
+if (pwd==null||pwd==''){  
+Lcolor=Mcolor=Hcolor=O_color;  
+}  
+else{  
+S_level=checkStrong(pwd);  
+switch(S_level) {  
+case 0:  
+Lcolor=Mcolor=Hcolor=O_color;  
+case 1:  
+Lcolor=L_color;  
+Mcolor=Hcolor=O_color;  
+break;  
+case 2:  
+Lcolor=Mcolor=M_color;  
+Hcolor=O_color;  
+break;  
+default:  
+Lcolor=Mcolor=Hcolor=H_color;  
+}  
+}  
+document.getElementById("strength_L").style.background=Lcolor;  
+document.getElementById("strength_M").style.background=Mcolor;  
+document.getElementById("strength_H").style.background=Hcolor;  
+return;  
+}  
+</script>
+</head>
+
+<body>
+<%@ include file="/inc/head-rgister.jsp"%>
+
+   <div class="regsiter-wrapper" id="register-form">
+     <div class="regsiter-box">
+        <div class="regsiter-wrapper-cnt">
+	         <ul>
+		         <li class="regsiter-title">账户注册</li>
+		         <li class="user">
+		         	<input type="text" name="phone" id="phone"class="int-xxlarge-user" placeholder="手机号码">
+		         	<p class="regsiter-po">
+			         	<span class="regsiter-note" id="errorPhoneMsg" style="display: none;">
+			         		<span id="showPhoneMsg" ></span>
+			         	</span>
+		         	</p>
+		         </li>
+		         
+		         <li class="password">
+		         	<input type="password" name="password" id="password"class="int-xxlarge" placeholder="密码"><!-- onKeyUp=pwStrength(this.value) onBlur=pwStrength(this.value) -->
+		         	<i class="icon-eye-open" id="togglePassword"></i>
+		         	<p class="regsiter-po">
+		         	<span class="regsiter-note" id="errorPawMsg" style="display: none;">
+		         		<span id="showPawMsg"></span>
+		         	</span>
+		         	</p>
+		         	<p>
+		         		<label id="errorShowPM" style="display: none;">6~14个字符，数字、字母、符号组合，不包含空格</label>
+		         	</p>
+		         	<div class="regsiter-set-password" style="display:none">
+				          <p class="low" id="strength_L">
+					          <span class="f00" id=""></span>
+					          <span>低</span>
+				          </p>
+				           <p class="in" id="strength_M">
+					          <span class="eb6100"></span>
+					          <span>中</span>
+				          </p>
+				          <p class="gao" id="strength_H">
+					          <span class="green"></span>
+					          <span>高</span>
+				          </p>
+	          			</div>	
+		         </li>
+		         	
+		         <li class="identifying">
+		         	<input type="text" class="int-xlarge-identifying" style="width:176px;" placeholder="图形验证码" id="pictureVitenfy">
+		         	<span ><A ><img src="${_base}/reg/getImageVerifyCode" id="randomImg"></A></span>
+		         	<span ><a id="refresh">看不清?换一个</a></span>
+		         	<p class="regsiter-po">
+			         	<span class="regsiter-note" id="errorPicMsg" style="display: none;">
+			         		<span id="showPicMsg" ></span>
+			         	</span>
+		         	</p>
+		         </li>
+		         <li class="SMSidentifying">
+		         	<input type="text" class="int-xlarge-SMSidentifying" placeholder="短信验证码" id="phoneVerifyCode">
+		         	 <span class="yzm">
+		         	 	<input class="button"id="PHONE_IDENTIFY"  type="button" value="获取验证码" >
+		         	 </span>
+		         		
+		         	<span class="regsiter-note" id="errorSmsMsg" style="display: none;">
+		         		<span id="showSmsMsg"></span>
+		         	</span>
+		         </li>
+		         <li>
+		         	<input type="button" class="regsiter-btn" value="注 册"  id="BTN_REGISTER">
+		         	<input type="hidden" id="errorPhoneFlag">
+		         	<input type="hidden" id="errorPicFlag">
+		         	<input type="hidden" id="errorPassFlag">
+		         	<input type="hidden" id="errorSMSFlag">
+		         </li>
+		         <li class="zuns">* 注册表示您同意遵守<A href="${_base }/reg/protocol" target="_blank">《亚信云计费服务条款》</A></li>
+	
+	         </ul>
+	         </div>
+   		</div>
+    </div>
+    <%@ include file="/inc/foot.jsp"%>
+   
+</body>
+</html>
---
+++ src/test/java/com/ai/opt/uac/web/test/ccs/IConfigCenterClientTest.java
@@ -0,0 +1,177 @@
+package com.ai.opt.uac.web.test.ccs;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.ai.opt.sdk.configcenter.client.IConfigCenterClient;
+import com.ai.opt.sdk.configcenter.factory.ConfigCenterFactory;
+import com.ai.opt.sdk.constants.SDKConstants;
+import com.ai.opt.uac.web.constants.Constants;
+import com.ai.opt.uac.web.constants.VerifyConstants;
+import com.ai.opt.uac.web.constants.VerifyConstants.EmailVerifyConstants;
+import com.ai.opt.uac.web.constants.VerifyConstants.PhoneVerifyConstants;
+import com.ai.opt.uac.web.constants.VerifyConstants.PictureVerifyConstants;
+
+public class IConfigCenterClientTest {
+
+    private IConfigCenterClient client;
+
+    @Before
+    public void initData() {
+        this.client = ConfigCenterFactory.getConfigCenterClient();
+    }
+
+    @Ignore
+    @Test
+    public void testGetConfig() throws Exception {
+        client.add("/test", "test");
+        assertEquals("test", client.get("/test"));
+        System.out.println("aaaaaa");
+    }
+
+    //@Ignore
+    @Test
+    public void addMcsConfig() {
+        // 缓存服务主机
+        String uacRedisHost = "uacRedisHost";
+        // 缓存空间
+        String cachesnsConfig = "{\"com.ai.opt.uac.sso.unicache\":\"" + uacRedisHost
+                + "\",\"com.ai.opt.uac.register.cache\":\"" + uacRedisHost
+                + "\",\""+Constants.RetakePassword.CACHE_NAMESPACE+"\":\"" + uacRedisHost
+                + "\",\""+Constants.UpdateEmail.CACHE_NAMESPACE+"\":\"" + uacRedisHost
+                + "\",\""+Constants.UpdatePhone.CACHE_NAMESPACE+"\":\"" + uacRedisHost
+                + "\",\""+Constants.UpdatePassword.CACHE_NAMESPACE+"\":\"" + uacRedisHost
+                + "\",\""+Constants.LoginConstant.CACHE_NAMESPACE+"\":\"" + uacRedisHost
+                + "\",\""+Constants.BandEmail.CACHE_NAMESPACE+"\":\"" + uacRedisHost
+                + "\",\"com.ai.opt.uni.session.sessionclient.uacweb\":\"" + uacRedisHost + "\"}";
+        
+        StringBuilder bu=new StringBuilder();
+        bu.append("{								");
+        bu.append("  \"uacRedisHost\":                     ");
+        bu.append("  {                                      ");
+        bu.append("		  \"mcsHost\":\"10.1.130.84:16379\",     ");
+        //bu.append("		  \"mcsHost\":\"localhost:6379\",     ");
+        bu.append("	  	\"mcsMaxtotal\":\"200\",            ");
+        bu.append("		  \"mcsMaxIdle\":\"10\",              ");
+        bu.append("		  \"mcsMinIdle\":\"5\",               ");
+        bu.append("		  \"mcsTestOnBorrow\":\"true\",       ");
+        bu.append("		  \"mcsPassword\":\"\"          ");
+        bu.append("  }                                     ");
+        bu.append("}                                        ");
+        
+        
+        
+        // 缓存服务主机和密码设置
+        if (!client.exists(
+                SDKConstants.PAAS_CACHE_REDIS_CLUSTER_MAPPED_PATH)) {
+            client.add(
+                    SDKConstants.PAAS_CACHE_REDIS_CLUSTER_MAPPED_PATH,
+                    bu.toString());
+        } else {
+            client.modify(
+                    SDKConstants.PAAS_CACHE_REDIS_CLUSTER_MAPPED_PATH,
+                    bu.toString());
+        }
+
+        // 缓存空间配置
+        if (!client.exists(SDKConstants.PAAS_CACHENS_MCS_MAPPED_PATH))
+            client.add(SDKConstants.PAAS_CACHENS_MCS_MAPPED_PATH,
+                    cachesnsConfig);
+        else {
+            client.modify(SDKConstants.PAAS_CACHENS_MCS_MAPPED_PATH,
+                    cachesnsConfig);
+        }
+    }
+    //@Ignore
+    @Test
+    public void readMcsConfig() {
+    	
+    	String cachesns=client.get(SDKConstants.PAAS_CACHENS_MCS_MAPPED_PATH);
+    	String redisconf=client.get(SDKConstants.PAAS_CACHE_REDIS_CLUSTER_MAPPED_PATH);
+    	
+    	System.out.println("cachesns:"+cachesns);
+    	System.out.println("redisconf:"+redisconf);
+    	
+    }
+
+    
+     @Test
+     public void addUrlConfig(){
+    	 System.out.println("url config ... start");
+    	 String indexUrl = "http://10.1.235.245:14101/baas-pt";
+    	 if (!client.exists(Constants.URLConstant.INDEX_URL_KEY)) {
+             client.add(Constants.URLConstant.INDEX_URL_KEY, indexUrl);
+         } else {
+             client.modify(Constants.URLConstant.INDEX_URL_KEY, indexUrl);
+         }
+
+    	 System.out.println("url config ... end");
+     }
+     
+     @Test
+     public void addSendVerifyTimesConfig(){
+    	 System.out.println("addSendVerifyTimesConfig ... start");
+    	 String PHONE_VERIFY_OVERTIME = "300";
+    	 String PHONE_SEND_VERIFY_MAX_TIME = "60";
+    	 String EMAIL_VERIFY_OVERTIME = "1800";
+    	 String EMAIL_SEND_VERIFY_MAX_TIME = "60";
+    	 String PICTURE_VERIFY_OVERTIME = "600";
+    	 String PHONE_IP_SEND_MAXTIMES="10";
+    	 String EMAIL_IP_SEND_MAXTIMES="10";
+    	 String PHONE_IP_SEND_OVERTIME="300";
+    	 String EMAIL_IP_SEND_OVERTIME="300";
+    	 if (!client.exists(VerifyConstants.PhoneVerifyConstants.SEND_VERIFY_MAX_TIME_KEY)) {
+             client.add(PhoneVerifyConstants.SEND_VERIFY_MAX_TIME_KEY, PHONE_SEND_VERIFY_MAX_TIME);
+         } else {
+             client.modify(PhoneVerifyConstants.SEND_VERIFY_MAX_TIME_KEY, PHONE_SEND_VERIFY_MAX_TIME);
+         }
+    	 if (!client.exists(VerifyConstants.PhoneVerifyConstants.VERIFY_OVERTIME_KEY)) {
+             client.add(PhoneVerifyConstants.VERIFY_OVERTIME_KEY, PHONE_VERIFY_OVERTIME);
+         } else {
+             client.modify(PhoneVerifyConstants.VERIFY_OVERTIME_KEY, PHONE_VERIFY_OVERTIME);
+         }
+    	 
+    	 if (!client.exists(VerifyConstants.EmailVerifyConstants.SEND_VERIFY_MAX_TIME_KEY)) {
+             client.add(EmailVerifyConstants.SEND_VERIFY_MAX_TIME_KEY, EMAIL_SEND_VERIFY_MAX_TIME);
+         } else {
+             client.modify(EmailVerifyConstants.SEND_VERIFY_MAX_TIME_KEY, EMAIL_SEND_VERIFY_MAX_TIME);
+         }
+    	 if (!client.exists(VerifyConstants.EmailVerifyConstants.VERIFY_OVERTIME_KEY)) {
+             client.add(EmailVerifyConstants.VERIFY_OVERTIME_KEY, EMAIL_VERIFY_OVERTIME);
+         } else {
+             client.modify(EmailVerifyConstants.VERIFY_OVERTIME_KEY, EMAIL_VERIFY_OVERTIME);
+         }
+    	 if (!client.exists(VerifyConstants.PictureVerifyConstants.VERIFY_OVERTIME_KEY)) {
+             client.add(PictureVerifyConstants.VERIFY_OVERTIME_KEY, PICTURE_VERIFY_OVERTIME);
+         } else {
+             client.modify(PictureVerifyConstants.VERIFY_OVERTIME_KEY, PICTURE_VERIFY_OVERTIME);
+         }
+    	 if (!client.exists(VerifyConstants.PhoneVerifyConstants.SEND_VERIFY_IP_MAX_NO_KEY)) {
+             client.add(PhoneVerifyConstants.SEND_VERIFY_IP_MAX_NO_KEY, PHONE_IP_SEND_MAXTIMES);
+         } else {
+             client.modify(PhoneVerifyConstants.SEND_VERIFY_IP_MAX_NO_KEY, PHONE_IP_SEND_MAXTIMES);
+         }
+    	 if (!client.exists(VerifyConstants.EmailVerifyConstants.SEND_VERIFY_IP_MAX_NO_KEY)) {
+             client.add(EmailVerifyConstants.SEND_VERIFY_IP_MAX_NO_KEY, EMAIL_IP_SEND_MAXTIMES);
+         } else {
+             client.modify(EmailVerifyConstants.SEND_VERIFY_IP_MAX_NO_KEY, EMAIL_IP_SEND_MAXTIMES);
+         }
+    	 
+    	 if (!client.exists(VerifyConstants.PhoneVerifyConstants.IP_SEND_OVERTIME_KEY)) {
+             client.add(PhoneVerifyConstants.IP_SEND_OVERTIME_KEY, PHONE_IP_SEND_OVERTIME);
+         } else {
+             client.modify(PhoneVerifyConstants.IP_SEND_OVERTIME_KEY, PHONE_IP_SEND_OVERTIME);
+         }
+    	 
+    	 if (!client.exists(VerifyConstants.EmailVerifyConstants.IP_SEND_OVERTIME_KEY)) {
+             client.add(EmailVerifyConstants.IP_SEND_OVERTIME_KEY, EMAIL_IP_SEND_OVERTIME);
+         } else {
+             client.modify(EmailVerifyConstants.IP_SEND_OVERTIME_KEY, EMAIL_IP_SEND_OVERTIME);
+         }
+    	 System.out.println("addSendVerifyTimesConfig ... end");
+     }
+    
+}
