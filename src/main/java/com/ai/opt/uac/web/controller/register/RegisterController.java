9c06b23a8828259b1d2c5a4b0294a3aea39fd5f7
zhangyh7 <zhangyh7@zhangyh7.ai.com>
2016-05-14 16:25:18 +0800

    注册

--- src/main/java/com/ai/opt/uac/web/controller/register/RegisterController.java
+++ src/main/java/com/ai/opt/uac/web/controller/register/RegisterController.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+ 
 import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
