656df92aeb89a85cb8ab8c9829de8cb28a9be4a3
Administrator <Administrator@WIN-20160614QQX>
2016-06-14 14:35:31 +0800

    合并

--- src/main/java/com/ai/opt/uac/web/controller/common/LoginJumpPageController.java
+++ src/main/java/com/ai/opt/uac/web/controller/common/LoginJumpPageController.java
@@ -6,11 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ai.opt.sdk.components.ccs.CCSClientFactory;
-import com.ai.opt.sdk.components.mcs.MCSClientFactory;
 import com.ai.opt.sdk.util.StringUtil;
 import com.ai.opt.sso.client.filter.SSOClientUtil;
 import com.ai.opt.uac.web.constants.Constants;
-import com.ai.opt.uac.web.constants.Constants.LoginConstant;
 import com.ai.paas.ipaas.ccs.constants.ConfigException;
 
 @RequestMapping("/home")
@@ -22,7 +20,8 @@ public class LoginJumpPageController {
 		//获取配置中心配置的登录页面
 		String service_url="";
         try {
-            service_url = CCSClientFactory.getDefaultConfigClient().get(Constants.URLConstant.INDEX_URL_KEY);
+        	//service_url目前配置的是商城的首页。因为商城首页不需登录，无法获取session，所以url后面加上/login
+            service_url = CCSClientFactory.getDefaultConfigClient().get(Constants.URLConstant.INDEX_URL_KEY)+"/login";
         } catch (ConfigException e) {
             e.printStackTrace();            
         }
